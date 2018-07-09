d60833a163dd7d6a42c6776df77aaa95ae0ef76f
Anousone Kaseumsouk, Jean Jia and Lan Dam <anousone@>
2018-07-09 14:38:59 -0700

    Add city and country query.

--- World/Controllers/CitiesController.cs
+++ World/Controllers/CitiesController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using World.Models;
+using MySql.Data.MySqlClient;
 
 namespace World.Controllers
 {
@@ -12,6 +13,37 @@ namespace World.Controllers
     {
         List<City> allCities = City.GetAll();
         return View(allCities);
+    }
+    [HttpGet("/cities/search")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+    [HttpPost("/cities")]
+    public ActionResult SearchCities(string citySearch)
+    {
+      List<City> allCities = new List<City> {};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM city WHERE name='" + citySearch + "';";
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while(rdr.Read())
+      {
+        int id = rdr.GetInt32(0);
+        string name = rdr.GetString(1);
+        string code = rdr.GetString(2);
+        string district = rdr.GetString(3);
+        int population = rdr.GetInt32(4);
+        City newCity = new City(id, name, code, district, population);
+        allCities.Add(newCity);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return View("Index", allCities);
     }
   }
 }
--- World/Controllers/CountriesController.cs
+++ World/Controllers/CountriesController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using World.Models;
+using MySql.Data.MySqlClient;
 
 namespace World.Controllers
 {
@@ -12,6 +13,38 @@ namespace World.Controllers
     {
         List<Country> allCountries = Country.GetAll();
         return View(allCountries);
+    }
+    [HttpGet("/countries/search")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+    [HttpPost("/countries")]
+    public ActionResult SearchCountries(string countrySearch)
+    {
+      List<Country> allCountries = new List<Country> {};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM country WHERE name='" + countrySearch + "';";
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while(rdr.Read())
+      {
+        string name = rdr.GetString(1);
+        string code = rdr.GetString(0);
+        string continent = rdr.GetString(2);
+        float area = rdr.GetFloat(4);
+        int population = rdr.GetInt32(6);
+        float gnp = rdr.GetFloat(8);
+        Country newCountry = new Country(name, code, continent, area, population, gnp);
+        allCountries.Add(newCountry);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return View("Index", allCountries);
     }
   }
 }
